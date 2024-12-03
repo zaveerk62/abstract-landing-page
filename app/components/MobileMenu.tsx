11433c833da0544ec294ce6d3aec9dbc112a55a5
Zaveer Khan <zaveerkhan.fire@gmail.com>
2024-12-04 03:36:23 +0500

    mobile menu fixes

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MobileMenu = () => {
   return (   
-      <div className='fixed left-14 md:left-28 top-20 z-50 w-[80%] h-[400px] bg-white shadow-lg flex items-center justify-center lg:hidden'>
+      <div className='fixed top-20 left-12 md:left-24 md:w-[80%] z-50 w-[50%] h-[400px] bg-white shadow-lg flex items-center justify-center lg:hidden'>
       <nav className='flex flex-col gap-4 p-6'>
       <a href="#" className="nav-item text-xl font-bold">Pricing</a>
       <a href="#" className="nav-item text-xl font-bold">Product</a>
